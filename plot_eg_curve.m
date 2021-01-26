ed30213ab4731501d0fd768eb6e06447115344d0
Tyler McCown <tylermccown@engineering.ucla.edu>
2021-01-26 15:12:37 -0800

    added engine curve from datasheet and incorporated plotting

--- plot_eg_curve.m
+++ plot_eg_curve.m
@@ -3,9 +3,23 @@ clc;
 pretty_pictures;
 
 load('eg_curve.mat');
+load('bs_m19_torque_curve.mat');
+speedVector_datasheet = bs_m19_torque_curve(:,1);
+torqueVector_datasheet = bs_m19_torque_curve(:,2);
 
-plot(speedVector, torqueVector);
-title('BS Vanguard 19 Torque vs Speed');
+powerVector = speedVector*2*pi/60.*torqueVector;
+powerVector_datasheet = speedVector_datasheet*2*pi/60.*torqueVector_datasheet;
+
+figure(4);
+sgtitle('BS Vanguard 19 Engine Curves');
+subplot(211); % torque
+plot(speedVector, torqueVector, speedVector_datasheet, torqueVector_datasheet);
 xlabel('RPM');
-ylabel('N-m');
+ylabel('Torque [Nm]');
+grid on;
+legend({'SAE India', 'B\&S Datasheet'}, 'location', 'southwest');
+subplot(212); % power
+plot(speedVector, powerVector/1000, speedVector_datasheet, powerVector_datasheet/1000);
+xlabel('RPM');
+ylabel('Power [kW]');
 grid on;
